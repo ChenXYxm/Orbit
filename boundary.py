9b1f21ab8f5e700559eb19c96600c7d084e9a948
cxy <louisechenxy@outlook.com>
2023-10-18 12:56:24 +0200

    new

---
+++ boundary.py
@@ -0,0 +1,228 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image as im 
+import cv2
+from scipy import ndimage
+def slice_occu(occu=np.array,tol=1):
+    
+    occu_tmp = occu.copy()
+    occu_ori = occu.copy()
+    (lx,ly) = occu.shape
+    index_tmp_e = dict()
+    index_tmp_s = dict()
+    shape_dict = dict()
+    shape_num = 0
+    for i in range(lx):
+        while len(np.where(occu_tmp[i]==1)[0]) >0:
+            flag = True
+            temp_s = np.where(occu_tmp[i]==1)[0][0]
+            temp_e = np.where(occu_tmp[i]==-1)[0][0]
+            shape_num +=1
+            shape_dict[shape_num] = [[i,temp_s]]
+            shape_dict[shape_num].append([i,temp_e])
+            occu_tmp[i,temp_s] = 0
+            occu_tmp[i,temp_e] = 0
+            i_d = i
+            while flag:
+                i_d +=1
+                if len(np.where(occu_tmp[i_d]==1)[0]) >0:
+                    # find closest continuous point
+                    diff_tmp = np.abs(np.where(occu_tmp[i_d]==1)[0] - temp_s)
+                    if len(np.where(diff_tmp<=tol)[0]) == 0:
+                        flag = False
+                    else:
+                        temp_s = np.where(occu_tmp[i_d]==1)[0][np.where(diff_tmp<=tol)[0][0]]
+                        temp_e = np.where(occu_tmp[i_d]==-1)[0][np.where(diff_tmp<=tol)[0][0]]
+                        shape_dict[shape_num].append([i_d,temp_s])
+                        shape_dict[shape_num].append([i_d,temp_e])
+                        occu_tmp[i_d,temp_s] = 0
+                        occu_tmp[i_d,temp_e] = 0
+                else:
+                    flag = False
+    print(shape_dict)
+    for i in shape_dict:
+        points_tmp = np.array(shape_dict[i],dtype=int)
+        bbox_min = np.min(points_tmp,axis=0)
+        bbox_max = np.max(points_tmp,axis=0)
+        occu_tmp[bbox_min[0]:bbox_max[0]+1,bbox_min[1]:bbox_max[1]+1] = -1
+    plt.imshow(occu_tmp)
+    plt.show()
+    
+        # print(points_tmp.shape)
+        # print(points_tmp)
+            # index_tmp_s[i] = np.where(occu_tmp[i]==1)[0]
+        # if len(np.where(occu_tmp[i]==-1)[0]) >0:
+        #     index_tmp_e[i] = np.where(occu_tmp[i]==-1)[0]
+    # i_s_y = -1
+    # i_s_x = -1
+    # i_e_y = -1
+    # i_e_x = -1
+    # num_cu = 0
+    # for i in range(lx):
+    #     if i_s_y == -1:
+    #         i_s_y = i
+    #         if len(index_tmp_s[i].reshape(-1))>1:
+    #             i_s_x = index_tmp_s[i][0]
+
+
+    
+
+
+# occu = np.load('occu.npy')
+# # print(occu)
+# occu_p = occu.copy()/np.max(occu)
+# occu_p = np.array(occu*255,dtype=np.uint8)
+# data = im.fromarray(occu_p) 
+
+# data.save('occu_1.png') 
+# (lx,ly) = occu.shape
+# occu_ori = occu.copy()
+# occu_t = np.zeros((lx+2,ly+2))
+# occu_t[1:-1,1:-1] = occu
+# occu = occu_t*-1
+# plt.imshow(occu)
+# plt.show()
+# # plt.savefig('occu.png')
+# occu_dx = np.zeros(occu.shape)
+# occu_dy = np.zeros(occu.shape)
+# occu_dx[1:] = np.diff(occu,axis=0)
+# occu_dy[:,1:] = np.diff(occu,axis=1)
+# slice_occu(occu_dy)
+# plt.figure()
+# plt.imshow(occu_dx)
+# plt.figure()
+# plt.imshow(occu_dy)
+# plt.figure()
+# plt.imshow(occu_dy+occu_dx)
+# plt.show()
+bbox = [0.07,0.2]
+num_grid_l = int(np.ceil(np.max(np.array(bbox))/0.01))
+num_grid_s = int(np.ceil(np.min(np.array(bbox))/0.01))
+mask = np.zeros((int(2*num_grid_l),int(2*num_grid_l)))
+mask[int(num_grid_l-np.ceil(num_grid_s/2)):int(num_grid_l+np.ceil(num_grid_s/2)),int(num_grid_l-np.ceil(num_grid_l/2)):int(num_grid_l+np.ceil(num_grid_l/2))] = 1
+vertices = [[int(num_grid_l-np.ceil(num_grid_s/2)),int(num_grid_l-np.ceil(num_grid_l/2))],
+            [int(num_grid_l-np.ceil(num_grid_s/2)),int(num_grid_l+np.ceil(num_grid_l/2))-1],
+            [int(num_grid_l+np.ceil(num_grid_s/2))-1,int(num_grid_l+np.ceil(num_grid_l/2))-1],
+            [int(num_grid_l+np.ceil(num_grid_s/2))-1,int(num_grid_l-np.ceil(num_grid_l/2))]]
+# for i in vertices:
+#     mask[i[0],i[1]] = 2
+plt.imshow(mask)
+plt.show()
+# mask_45 = np.array(ndimage.rotate(mask,45,reshape=False))
+# print(np.max(mask_45))
+# print(mask_45.shape)
+# print(np.where(mask_45>1))
+# mask_45[np.where(mask_45>=0.4)] = 1
+# mask_45[np.where(mask_45<1)] = 0
+# print(len(np.where(mask>=1)[0]))
+# print(len(np.where(mask_45>=1)[0]))
+# plt.imshow(mask_45)
+# plt.show()
+
+img = cv2.imread('occu.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(gray,50,255,0)
+occu = np.array(thresh)/np.max(np.array(thresh))
+plt.imshow(occu)
+plt.show()
+print(thresh)
+contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+print("Number of contours detected:",len(contours))
+shape_dict = dict()
+i = 0
+for cnt in contours:
+    i += 1
+    # approx = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
+    # approx = cv2.convexHull(cnt)
+    approx = cv2.minAreaRect(cnt)
+    (x,y) = cnt[0,0]
+    # print(approx)
+    box = cv2.boxPoints(approx)
+    approx = np.int0(box)
+    if len(approx) >=2:
+        # img = cv2.drawContours(img,[approx],-1,(0,255,255),3)
+        print(approx)
+        approx = approx.reshape((-1,2))
+        print(approx)
+        shape_dict[i] = approx
+length_dict = dict()
+length_list = []
+occu_tmp = np.array(occu)
+for i in shape_dict:
+    points_tmp = shape_dict[i].copy()
+    points_tmp[:,0] = points_tmp[:,1]
+    points_tmp[:,1] = shape_dict[i][:,0].copy()
+    for j in range(len(points_tmp)):
+        p_s = points_tmp[j]
+        if j < len(points_tmp)-1:
+            p_e = points_tmp[j+1]
+        else:
+            p_e = points_tmp[0]
+        line = p_e - p_s
+        length = np.linalg.norm(line)
+        length_dict[length] = [p_s,p_e]
+        length_list.append(length)
+        print(p_s,p_e,length)
+        for k in range(int(np.ceil(length))):
+            occu_tmp[int(np.ceil(p_s[0]+k*line[0]/length)),int(np.ceil(p_s[1]+k*line[1]/length))] = 2
+plt.imshow(occu_tmp)
+plt.show()
+length_arr = abs(np.array(length_list)-num_grid_l)
+flag = False
+print(length_dict)
+for i in range(len(length_list)):
+    ind_tmp = np.argmin(length_arr)
+    p_s = length_dict[length_list[ind_tmp]][0]
+    p_e = length_dict[length_list[ind_tmp]][1]
+    print("points")
+    print(p_s,p_e)
+    line = np.array(p_e) - np.array(p_s)
+    length = np.linalg.norm(line)
+    print(line,length)
+    angle = (np.arccos(line[1]/length))/np.pi*180
+    print(angle)
+    # angle = 90
+    mask_45 = np.array(ndimage.rotate(mask,angle,reshape=False))
+    
+    mask_45[np.where(mask_45>=0.4)] = 1
+    mask_45[np.where(mask_45<1)] = 0
+    index = np.where(mask_45>=1)
+    index_arr = np.zeros((len(index[0]),2))
+    index_arr[:,0] = index[0]
+    index_arr[:,1] = index[1]
+    index_arr = np.array(index_arr,dtype=int).reshape((-1,2))
+
+    vertices = [index_arr[np.where(index_arr[:,1]==np.min(index_arr[:,1]))[0]].reshape(-1),
+                index_arr[np.where(index_arr[:,0]==np.max(index_arr[:,0]))[0]].reshape(-1),
+                index_arr[np.where(index_arr[:,1]==np.max(index_arr[:,1]))[-1]].reshape(-1),
+                index_arr[np.where(index_arr[:,0]==np.min(index_arr[:,0]))[0]].reshape(-1)]
+    
+    print(vertices)
+    # for i in vertices:
+    #     print(i)
+    #     mask_45[i[0],i[1]] = 2
+    plt.imshow(mask_45)
+    plt.show()
+    occu_ori = occu.copy()
+    print(p_e,p_s)
+    print(mask_45.shape)
+    print(2*num_grid_l)
+    occu_ori[29:60,14:54] = occu_ori[29:60,14:54] + mask_45[:31,]
+    plt.imshow(occu_ori)
+    plt.show()
+    flag = True
+    occu_ori2 = np.zeros(occu_ori.shape)
+    if flag:
+        break
+#     for k in range(int(np.ceil(length))):
+#         occu_ori2[int(np.ceil(p_s[0]+k*line[0]/length)),int(np.ceil(p_s[1]+k*line[1]/length))] =1
+# plt.imshow(occu_ori2)
+# plt.show()
+
+
+
+        # cv2.putText(img,'Polygon',(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.6,(255,255,0),2)
+# cv2.imshow("Polygon",img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
--- source/standalone/demo/play_ma.py
+++ source/standalone/demo/play_ma.py
@@ -24,17 +24,17 @@ simulation_app = SimulationApp(config)
 
 
 """Rest everything follows."""
-import os
+from PIL import Image as im 
+# import os
 import torch
 import cv2
 import numpy as np
 import open3d as o3d
-from pxr import Gf, UsdGeom
 import random
 import scipy.ndimage as ndimage
 from scipy import signal
 import omni.isaac.debug_draw._debug_draw as omni_debug_draw
-from omni.isaac.core.prims import RigidPrim
+from omni.isaac.core.prims import RigidPrim,GeometryPrim
 from omni.isaac.orbit.utils.array import convert_to_torch
 
 from pxr import Gf, UsdGeom
@@ -58,6 +58,7 @@ from omni.isaac.core.utils import prims
 from omni.isaac.core.objects import DynamicCuboid
 from PIL import Image
 import matplotlib.pyplot as plt
+import omni.isaac.core.utils.semantics as semantics_utils
 import omni.isaac.core.utils.numpy.rotations as rot_utils
 from omni.isaac.orbit.controllers.differential_inverse_kinematics import (
     DifferentialInverseKinematics,
@@ -108,6 +109,7 @@ def point_cloud_process(pcd):
     # [a, b, c, d] = plane_model
     inlier_cloud = outlier_cloud.select_by_index(inliers)
     # o3d.visualization.draw_geometries([inlier_cloud])
+    print(plane_model)
     return plane_model
 
 def main():
@@ -117,7 +119,7 @@ def main():
     # sim = SimulationContext(physics_dt=0.01, rendering_dt=0.01)
     sim = SimulationContext(physics_dt=0.01, rendering_dt=0.01, backend="torch",device='cuda:0')
     # Set main camera
-    set_camera_view([0, 1, 3.], [0.0, 0.0, 0])
+    set_camera_view([0, 2, 3.], [0.0, 0.0, 0])
     # Enable GPU pipeline and flatcache
     if sim.get_physics_context().use_gpu_pipeline:
         sim.get_physics_context().enable_flatcache(True)
@@ -159,6 +161,8 @@ def main():
     Table = DynamicCuboid(prim_path="/World/Table",position=(0,0,-0.25),scale=(1,0.6,0.5))
     
     Table.set_mass(10000000) 
+    sideTable = DynamicCuboid(prim_path="/World/sideTable",position=(0.35,-0.9,-0.3),scale=(0.4,0.4,0.4))
+    sideTable.set_mass(10)
     #################### robot base
     prim_utils.create_prim("/World/Robotbase", usd_path=table_path,position=(0,-0.45,-0.2),scale=(0.3,0.26,0.4))
     #################### ycb path
@@ -167,8 +171,10 @@ def main():
         "sugarBox": f"{ISAAC_NUCLEUS_DIR}/Props/YCB/Axis_Aligned_Physics/004_sugar_box.usd",
         "tomatoSoupCan": f"{ISAAC_NUCLEUS_DIR}/Props/YCB/Axis_Aligned_Physics/005_tomato_soup_can.usd",
         "mustardBottle": f"{ISAAC_NUCLEUS_DIR}/Props/YCB/Axis_Aligned_Physics/006_mustard_bottle.usd",
+        "mug":f"{ISAAC_NUCLEUS_DIR}/Props/YCB/Axis_Aligned/025_mug.usd",
+        "largeMarker":f"{ISAAC_NUCLEUS_DIR}/Props/YCB/Axis_Aligned/040_large_marker.usd",
     }
-    ycb_name = ['crackerBox','sugarBox','tomatoSoupCan','mustardBottle']
+    ycb_name = ['crackerBox','sugarBox','tomatoSoupCan','mustardBottle','mug','largeMarker']
     
     
     ################################ robot setting
@@ -205,9 +211,7 @@ def main():
     # print(intrinsics_m)
     
     ###################################### rep camera
-    
-    image_pixel = [512,256]
-
+    # image_pixel = [512,256]
     # cam = rep.create.camera(position=(0,0,5), look_at=(0,0,0))
     # rp = rep.create.render_product(cam, image_pixel)
     camera_cfg = PinholeCameraCfg(
@@ -251,9 +255,16 @@ def main():
         sim.render()
     ##################################################################### load ycb
     for key, usd_path in ycb_usd_paths.items():
+        
         translation = torch.rand(3).tolist()
-        translation = [translation[0]*0.8-0.4,0.45*translation[1]-0.225,0.1]
-        prim_utils.create_prim(f"/World/Objects/{key}", usd_path=usd_path, translation=translation)
+        translation = [translation[0]*0.8-0.4,0.45*translation[1]-0.225,0.06]
+        # translation = [translation[0]*0.8-0.4,0.8*translation[1]+0.5,0.1]
+        rot = convert_quat(tf.Rotation.from_euler("XYZ", (0,0,translation[0]*90), degrees=True).as_quat(), to="wxyz")
+        if key in ["mug","tomatoSoupCan"]:
+           rot = convert_quat(tf.Rotation.from_euler("XYZ", (-90,0,translation[0]*90), degrees=True).as_quat(), to="wxyz")
+        prim_utils.create_prim(f"/World/Objects/{key}", usd_path=usd_path, translation=translation,orientation=rot)
+        GeometryPrim(f"/World/Objects/{key}",collision=True)
+        RigidPrim(f"/World/Objects/{key}",mass=0.5)
     ##################################################################### 
     
     # rep.orchestrator.step()
@@ -316,7 +327,7 @@ def main():
         print('running')
     # Set end effector goals
     # Define goals for the arm
-    ee_goals = [0, 0.5, 1, 0.0, 0, 0.707, 0]
+    ee_goals = [0.2, -0.95, 1.0, 0.0, 0, 0.0, 0]
     ee_goals = torch.tensor(ee_goals, device=sim.device)
     # Track the given command
     current_goal_idx = 0
@@ -335,6 +346,11 @@ def main():
     count = 0
     plane_model_ori = plane_model
     plane_model = np.array([plane_model[0],plane_model[1],plane_model[2]])
+    # rep_camera = rep.create.render_product("/World/CameraSensor",resolution=(640,480))
+    # rep_annotator = rep.AnnotatorRegistry.get_annotator("bounding_box_2d_loose")
+    # rep_annotator.attach([rep_camera])
+    num_new = 0
+    hand_plane_model = None
     while simulation_app.is_running():
         # If simulation is stopped, then exit.
         if simulation_app.is_running():
@@ -347,6 +363,7 @@ def main():
             print('pause')
             sim.play()
             continue
+        ########################################## ik control
         # perform step
         # set the controller commands
         ik_controller.set_command(ik_commands)
@@ -379,7 +396,7 @@ def main():
             robot.update_buffers(sim_dt)
             count +=1
             # update marker positions
-        if count >=20:
+        if count >=50:
             count = 0
             camera.update(dt=0.0)
             hand_camera.update(dt=0.0)
@@ -405,7 +422,7 @@ def main():
             # print(camera.data.output["distance_to_image_plane"].shape)
             # print(pointcloud_w.shape)
             select_m = np.dot(inliers,plane_model) - float(plane_model_ori[3])
-            index_objects = np.argwhere((select_m>=0.02)).reshape(-1).astype(int)
+            index_objects = np.argwhere((select_m>=0.01)).reshape(-1).astype(int)
             objects_point = inliers[index_objects].copy()
             objects_pcd = o3d.geometry.PointCloud()
             objects_pcd.points = o3d.utility.Vector3dVector(objects_point)
@@ -426,7 +443,6 @@ def main():
             occupancy = np.zeros( (Ny,Nx) )
             u = u.astype(int)
             v = v.astype(int)
-        #     print(u,v)
             u_ind = np.where(u<100)
             u = u[u_ind]
             v = v[u_ind]
@@ -441,32 +457,165 @@ def main():
             v = v[v_ind]
             occupancy[v,u] = 1
             occupancy = np.fliplr(occupancy)
-
             # plt.imshow(occupancy)
             # plt.show()
-            structure = np.ones((3,3))
-            occupancy = ndimage.binary_dilation(occupancy,structure=structure)
-            grad = boundary_detection(occupancy)
+            # np.save("occu",occupancy)
+            # structure = np.ones((3,3))
+            # occupancy = ndimage.binary_dilation(occupancy,structure=structure)
+            # grad = boundary_detection(occupancy)
+            bound_detect(occupancy)
             rgb=camera.data.output["rgb"]
             rgb = convert_to_torch(rgb, device=sim.device, dtype=torch.float32)
             rgb = rgb[:, :, :3].cpu().data.numpy()
-            # print(rgb)
+            # plt.imshow(grad)
+            # plt.show()
             img = Image.fromarray((rgb).astype(np.uint8))
             hand_rgb=hand_camera.data.output["rgb"]
             hand_rgb = convert_to_torch(hand_rgb, device=sim.device, dtype=torch.float32)
             hand_rgb = hand_rgb[:, :, :3].cpu().data.numpy()
+            place_new_object(occupancy,ycb_name,ycb_usd_paths,num_new)
+            num_new +=1
             hand_img = Image.fromarray((hand_rgb).astype(np.uint8))
+            for _ in range(25):
+                sim.render()
+                sim.step()
+                sim.play()
+                camera.update(dt=0.0)
+                hand_camera.update(dt=0.0)
+                
             plt.imshow(img)
             plt.show()
-            plt.imshow(hand_img)
-            plt.show()
-            
+            # plt.imshow(hand_img)
+            # plt.show()
+            if num_new>=1:
+                aabb_points = get_new_obj_pcd(hand_camera,(40,40),hand_plane_model)
+            # bbox = rep_annotator.get_data()
+            # print(bbox)
         sim.step()
-
+def get_new_obj_pcd(camera,size,hand_plane_model):
+    pointcloud_w, pointcloud_rgb = create_pointcloud_from_rgbd(
+                    camera.data.intrinsic_matrix,
+                    depth=camera.data.output["distance_to_image_plane"],
+                    rgb=camera.data.output["rgb"],
+                    position=camera.data.position,
+                    orientation=camera.data.orientation,
+                    normalize_rgb=True,
+                    num_channels=4,
+    )
+    if not isinstance(pointcloud_w, np.ndarray):
+        pointcloud_w = pointcloud_w.cpu().numpy()
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(pointcloud_w)
+    # o3d.visualization.draw_geometries([pcd])
+    plane_model, inliers = pcd.segment_plane(distance_threshold=0.01,
+                                         ransac_n=3,
+                                         num_iterations=1000)
+    outlier_cloud = pcd.select_by_index(inliers, invert=True)
+    plane_model, inliers = outlier_cloud.segment_plane(distance_threshold=0.005,
+                                         ransac_n=3,
+                                         num_iterations=1000)
+    inlier_cloud = outlier_cloud.select_by_index(inliers)
+    plane_model_ori = plane_model
+    plane_model = np.array([plane_model[0],plane_model[1],plane_model[2]])
+    pointcloud_w = np.array(outlier_cloud.points)
+    # select_m = np.dot(pointcloud_w,plane_model) - float(plane_model_ori[3])
+    # index_inliers = np.argwhere((select_m >=-0.1)).reshape(-1).astype(int)
+    # inliers = pointcloud_w[index_inliers]
+    # pcd = o3d.geometry.PointCloud()
+    # pcd.points = o3d.utility.Vector3dVector(outlier_cloud)
+    o3d.visualization.draw_geometries([inlier_cloud])
+    o3d.visualization.draw_geometries([outlier_cloud])
+    # select_m = np.dot(inliers,plane_model) - float(plane_model_ori[3])
+    # index_inliers = np.argwhere((select_m <=0.3)).reshape(-1).astype(int)
+    # inliers = inliers[index_inliers]
+    # index_inliers = np.argwhere((inliers[:,1]>=-0.3)).reshape(-1).astype(int)
+    # inliers = inliers[index_inliers]
+    # # print(camera.data.output["distance_to_image_plane"].shape)
+    # # print(pointcloud_w.shape)
+    select_m = np.dot(pointcloud_w,plane_model) + float(plane_model_ori[3])
+    index_objects = np.argwhere((select_m>=0.01)).reshape(-1).astype(int)
+    objects_point = pointcloud_w[index_objects].copy()
+    # objects_pcd = outlier_cloud.select_by_index(inliers,invert = True)
+    objects_pcd = o3d.geometry.PointCloud()
+    objects_pcd.points = o3d.utility.Vector3dVector(objects_point)
+    o3d.visualization.draw_geometries([objects_pcd])
+    pcd = inlier_cloud
+    # o3d.visualization.draw_geometries([pcd])
+    pts_tab = np.array(pcd.points)
+    # pts_tab[:,2] = 0
+    # pcd.points = o3d.utility.Vector3dVector(pts_tab)
+    aabb = objects_pcd.get_oriented_bounding_box()
+    print(np.array(aabb.get_box_points()))
+    aabb_points = np.array(aabb.get_box_points()).reshape((-1,3))
+    aabb.color = (1, 0, 0)
+    # o3d.visualization.draw_geometries([objects_pcd, aabb])
+    return aabb_points
+def place_new_object(occu,ycb_list,ycb_path,num_new):
+    random_i = random.randint(0,len(ycb_list)-1)
+    translation = [0.35,-0.9,0.05]
+    # translation = [translation[0]*0.8-0.4,0.8*translation[1]+0.5,0.1]
+    key = ycb_list[random_i]
+    # key = 'largeMarker'
+    usd_path = ycb_path[key]
+    if key in ["mug","tomatoSoupCan"]:
+        rot = convert_quat(tf.Rotation.from_euler("XYZ", (-90,0,0), degrees=True).as_quat(), to="wxyz")
+    else:  
+        rot = convert_quat(tf.Rotation.from_euler("XYZ", (0,0,0), degrees=True).as_quat(), to="wxyz")
+    prim_utils.create_prim(f"/World/newObjects/{key+str(num_new)}", usd_path=usd_path, translation=translation,orientation=rot)
+    GeometryPrim(f"/World/newObjects/{key+str(num_new)}",collision=True)
+    RigidPrim(f"/World/newObjects/{key+str(num_new)}",mass=0.5)
+    # prim_utils.delete_prim(f"/World/newObjects/{key+str(num_new)}")
+    
+def bound_detect(occu):
+    occu_p = occu.copy()/np.max(occu)
+    occu_p = np.array(occu*255,dtype=np.uint8)
+    data = im.fromarray(occu_p) 
+    data.save('occu.png') 
+    img = cv2.imread('occu.png')
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(gray,50,255,0)
+    contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    print("Number of contours detected:",len(contours))
+    shape_dict = dict()
+    i = 0
+    for cnt in contours:
+        i += 1
+        # approx = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
+        # approx = cv2.convexHull(cnt)
+        approx = cv2.minAreaRect(cnt)
+        (x,y) = cnt[0,0]
+        # print(approx)
+        box = cv2.boxPoints(approx)
+        approx = np.int0(box)
+        # print(box)
+        if len(approx) >=2:
+            # img = cv2.drawContours(img,[approx],-1,(0,255,255),3)
+            # print(approx)
+            approx = np.array(approx).reshape((-1,2))
+            
+            shape_dict[i] = approx
+    for i in shape_dict:
+        points_tmp = shape_dict[i].copy()
+        points_tmp[:,0] = points_tmp[:,1]
+        points_tmp[:,1] = shape_dict[i][:,0].copy()
+        for j in range(len(points_tmp)):
+            p_s = points_tmp[j]
+            if j < len(points_tmp)-1:
+                p_e = points_tmp[j+1]
+            else:
+                p_e = points_tmp[0]
+            line = p_e - p_s
+            length = np.linalg.norm(line)
+            # print(p_s,p_e,length)
+            for k in range(int(np.ceil(length))):
+                if np.ceil(p_s[0]+k*line[0]/length) < occu.shape[0] and np.ceil(p_s[1]+k*line[1]/length)<occu.shape[1] and np.ceil(p_s[1]+k*line[1]/length)>=0 and np.ceil(p_s[0]+k*line[0]/length)>=0:
+                    occu[int(np.ceil(p_s[0]+k*line[0]/length)),int(np.ceil(p_s[1]+k*line[1]/length))] = 2
+    plt.imshow(occu)
+    plt.show()
 def boundary_detection(occu):
     kernal = np.ones((3,3))
     kernal = kernal *float(1./9.)
-    grad = signal.convolve2d(occu,kernal)
+    grad = signal.convolve2d(occu,kernal,mode='same')
     index_bound = np.argwhere(grad>0)
     # print(index_bound)
     index_bound = index_bound.reshape((-1,2))
@@ -476,15 +625,20 @@ def boundary_detection(occu):
     index2 = np.argwhere(grad_bound<0.4).reshape((-1)).astype(int)
     # print(index2)
     index_bound = index_bound[index2]
-    grad_temp = np.zeros(grad.shape)
+    grad_temp = np.zeros(occu.shape)
+    # print('conv shape')
     # print(grad_temp.shape)
     # print(grad.shape)
     grad_temp[index_bound[:,0],index_bound[:,1]] = 1
+    # print(index_bound)
     # plt.imshow(grad_temp)
     # plt.show()
-    
     # grad[index_bound[:,0],index_bound[:,1]] = 2
-    return grad
+    return grad_temp
+def find_edges(points=np.array):
+    points_tmp = points.copy()
+    for i in range(len(points)):
+        [point_x,point_y] = points_tmp[i]
 
 if __name__ == "__main__":
     # Run empty stage
